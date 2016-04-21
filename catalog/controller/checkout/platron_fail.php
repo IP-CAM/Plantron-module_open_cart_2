36fa048902b7293c3d1fc15e2a77f25a74fe2ac0
FreeR <freer@mail.ru>
2016-04-22 02:28:13 +0300

    edit success and fail page

--- catalog/controller/checkout/platron_fail.php
+++ catalog/controller/checkout/platron_fail.php
@@ -1,93 +1,52 @@
 <?php
 class ControllerCheckoutPlatronFail extends Controller { 
 	public function index() { 	
-		
-		if ( isset($this->session->data['order_id']) && ( ! empty($this->session->data['order_id']))  ) {
-			$this->session->data['last_order_id'] = $this->session->data['order_id'];
-		}
-		
-		if (isset($this->session->data['order_id'])) {
-			$this->cart->clear();
-			
-			unset($this->session->data['shipping_method']);
-			unset($this->session->data['shipping_methods']);
-			unset($this->session->data['payment_method']);
-			unset($this->session->data['payment_methods']);
-			unset($this->session->data['guest']);
-			unset($this->session->data['comment']);
-			unset($this->session->data['order_id']);	
-			unset($this->session->data['coupon']);
-			unset($this->session->data['reward']);
-			unset($this->session->data['voucher']);
-			unset($this->session->data['vouchers']);
-		}	
-									   
-		$this->language->load('checkout/platron_fail');
-		
-		if (! empty($this->session->data['last_order_id']) ) {
-			$this->document->setTitle(sprintf($this->language->get('heading_title_customer'), $this->session->data['last_order_id']));
-		} else {
-    		$this->document->setTitle($this->language->get('heading_title'));
-		}
-		
-		$this->data['breadcrumbs'] = array(); 
+		$this->load->language('checkout/failure');
 
-      	$this->data['breadcrumbs'][] = array(
-        	'href'      => $this->url->link('common/home'),
-        	'text'      => $this->language->get('text_home'),
-        	'separator' => false
-      	); 
-		
-      	$this->data['breadcrumbs'][] = array(
-        	'href'      => $this->url->link('checkout/cart'),
-        	'text'      => $this->language->get('text_basket'),
-        	'separator' => $this->language->get('text_separator')
-      	);
-				
-		$this->data['breadcrumbs'][] = array(
-			'href'      => $this->url->link('checkout/checkout', '', 'SSL'),
-			'text'      => $this->language->get('text_checkout'),
-			'separator' => $this->language->get('text_separator')
-		);	
-					
-      	$this->data['breadcrumbs'][] = array(
-        	'href'      => $this->url->link('checkout/platron_fail'),
-        	'text'      => $this->language->get('text_fail'),
-        	'separator' => $this->language->get('text_separator')
-      	);
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		if (! empty($this->session->data['last_order_id']) ) {
-			$this->data['heading_title'] = sprintf($this->language->get('heading_title_customer'), $this->session->data['last_order_id']);
-		} else {
-    		$this->data['heading_title'] = $this->language->get('heading_title');
-		}
-		
-		if ($this->customer->isLogged()) {
-			$this->data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/order/info&order_id=' . $this->session->data['last_order_id'], '', 'SSL'), $this->session->data['last_order_id'], $this->url->link('account/account', '', 'SSL'), $this->url->link('account/order', '', 'SSL'), $this->url->link('account/download', '', 'SSL'), $this->url->link('information/contact'));
-		} else {
-    		$this->data['text_message'] = sprintf($this->language->get('text_guest'), $this->session->data['last_order_id'], $this->url->link('information/contact'));
-		}
-		
-    	$this->data['button_continue'] = $this->language->get('button_continue');
+		$data['breadcrumbs'] = array();
 
-    	$this->data['continue'] = $this->url->link('common/home');
-
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/platron_fail.tpl')) {
-			$this->template = $this->config->get('config_template') . '/template/common/platron_fail.tpl';
-		} else {
-			$this->template = 'default/template/common/platron_fail.tpl';
-		}
-		
-		$this->children = array(
-			'common/column_left',
-			'common/column_right',
-			'common/content_top',
-			'common/content_bottom',
-			'common/footer',
-			'common/header'			
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
 		);
-				
-		$this->response->setOutput($this->render());
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_basket'),
+			'href' => $this->url->link('checkout/cart')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_checkout'),
+			'href' => $this->url->link('checkout/checkout', '', 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_failure'),
+			'href' => $this->url->link('checkout/failure')
+		);
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['text_message'] = sprintf($this->language->get('text_message'), $this->url->link('information/contact'));
+
+		$data['button_continue'] = $this->language->get('button_continue');
+
+		$data['continue'] = $this->url->link('common/home');
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/success.tpl')) {
+			$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/success.tpl', $data));
+		} else {
+			$this->response->setOutput($this->load->view('default/template/common/success.tpl', $data));
+		}
   	}
 }
 ?>
